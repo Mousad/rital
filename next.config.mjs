3fddb52f8cad2156e405f51c3d1ba1dd442650a2
YourNewUsername <yournewemail@example.com>
2026-05-15 03:22:37 +0300

    fix images optimization

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,13 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96],
+    minimumCacheTTL: 60,
   },
 }
 
 export default nextConfig
+
+
